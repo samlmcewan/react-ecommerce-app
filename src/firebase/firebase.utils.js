803708b20486c2e807ab74db394c7581c690b398
samlmcewan <samlmcewan@gmail.com>
2020-09-03 14:58:59 +0100

    implementing firebase utils, including ability to add authorised users to firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
 	measurementId: 'G-QJQE469XEM'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	// check for auth object, return from function if not
+	if (!userAuth) return;
+	// if auth object does exist, query firestore for document/auth object with uid property
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+	// check if user data exists in firestore, if user doesn't exist create data there using data from userAuth object
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
